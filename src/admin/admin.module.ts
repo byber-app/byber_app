bb1e00fed6d5975ae403704e6d86bc3c2f2095eb
NamigGuliyef <quliyevnamiq8@gmail.com>
2025-08-02 17:31:28 +0400

    admin module fix

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -5,11 +5,16 @@ import { AdminService } from './admin.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Advertisement, AdvertisementModel } from 'src/advertisement/model/advertisement.schema';
 import { Service, ServiceModel } from 'src/service/model/service.schema';
+import { Barber, BarberModel } from 'src/barber/model/barber.schema';
+import { User, userModel } from 'src/user/model/user.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([
     { name: Advertisement.name, schema: AdvertisementModel },
-    { name: Service.name, schema: ServiceModel }])],
+    { name: Service.name, schema: ServiceModel },
+    { name: Barber.name, schema: BarberModel },
+    { name: User.name, schema: userModel },
+  ])],
   controllers: [AdminController],
   providers: [AdminService],
 })
